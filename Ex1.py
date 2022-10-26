ea9a193529010ef8c9b8abc085e86ab17a9c2917
Yong Quan <yongquan6993@gmail.com>
2022-10-26 18:11:11 +0800

    Second Commit

---
+++ Ex1.py
@@ -0,0 +1,10 @@
+# Exercise 1
+def calculate_bmi(height, weight):
+    print("\u0332".join("Exercise 1"))
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+
+calculate_bmi(weight=57, height=1.73)
+
+
